6e6072057f16648797deb8fee305dc216a15fd83
debendra.sahu <debendrasahu150@gmail.com>
2025-01-22 22:09:47 +0530

    Build a simple app with two Text Composables and a Button. When the button is clicked, update the text dynamically.

--- Day1/ComposeHelloWorld/app/src/main/java/com/debend/composehelloworld/MainActivity.kt
+++ Day1/ComposeHelloWorld/app/src/main/java/com/debend/composehelloworld/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.debend.composehelloworld.ui.theme.ComposeHelloWorldTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +25,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeHelloWorldTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    HomeScreen()
                 }
             }
         }
@@ -31,17 +34,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun HomeScreen() {
+    var text by remember { mutableStateOf("Click a button") }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeHelloWorldTheme {
-        Greeting("Android")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text)
+
+        Button(onClick = { text = "Button 1 Clicked" }) {
+            Text(text = "Button 1")
+        }
+        Button(onClick = { text = "Button 2 Clicked" }) {
+            Text(text = "Button 2")
+        }
     }
 }
