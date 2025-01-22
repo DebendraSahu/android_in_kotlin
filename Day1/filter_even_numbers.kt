514cd474235c41d63d0dd47955d2653682037984
DEBENDRA PRASAD SAHU <92933669+DebendraSahu@users.noreply.github.com>
2025-01-22 19:34:06 +0530

    Update filter_even_numbers.kt
     next task
    square them, and sort the result

--- Day1/filter_even_numbers.kt
+++ Day1/filter_even_numbers.kt
@@ -1,11 +1,12 @@
-// filter even numbers
-fun filterOutEvenNumbers(numbers: List<Int>): List<Int> {
-    return numbers.filter {it % 2 == 0 }
-}
-
+import kotlin.random.Random
+// filter even numbers, square them, and sort the result.
 fun main() {
-    val numbers = (0..100).toList()
-    val evenNumbers = filterOutEvenNumbers(numbers)
+    val numbers = List(50) { Random.nextInt(0, 200) }
     println("numbers $numbers")
+    val evenNumbers = numbers.filter {it % 2 == 0 }
     println("even numbers $evenNumbers")
+    val squaresOfEven = evenNumbers.map { it * it }
+    println("squares of even numbers $squaresOfEven")
+    val sortedSquaresOfEven = squaresOfEven.sorted() 
+    println("squares of even numbers sorted in ascending order $sortedSquaresOfEven")
 }
