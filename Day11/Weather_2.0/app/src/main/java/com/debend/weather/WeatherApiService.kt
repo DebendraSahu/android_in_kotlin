0fef1eadc6d882581b0dae67d62f8634d83bcb34
debendra.sahu <debendrasahu150@gmail.com>
2025-02-01 21:26:49 +0530

    Created Weather app v 2.0 from the older one.

---
+++ Day11/Weather_2.0/app/src/main/java/com/debend/weather/WeatherApiService.kt
@@ -0,0 +1,13 @@
+package com.debend.weather
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherApiService {
+    @GET("data/2.5/weather")
+    suspend fun getWeather(
+        @Query("q") city: String,
+        @Query("units") units: String = "metric",
+        @Query("appid") apiKey: String
+    ): WeatherResponse
+}
