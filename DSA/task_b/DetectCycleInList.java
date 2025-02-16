9d4e44074c6c16027d60e385ce01be0718096f06
debendra.sahu <debendrasahu150@gmail.com>
2025-02-16 23:58:43 +0530

    Added a problem to detect cycle in linked list

---
+++ DSA/task_b/DetectCycleInList.java
@@ -0,0 +1,38 @@
+package task_b;
+
+public class DetectCycleInList {
+    private class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        boolean hasCycle = false;
+
+        // Step 1: Detect cycle
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                hasCycle = true;
+                break;
+            }
+        }
+
+        if (!hasCycle) return null;
+
+        // Step 2: Find cycle start
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow;
+    }
+}
