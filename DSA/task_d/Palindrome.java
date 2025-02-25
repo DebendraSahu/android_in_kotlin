e19578571e73ffc69e060efadd855ac8b47e6948
debendra.sahu <debendrasahu150@gmail.com>
2025-02-25 23:19:57 +0530

    check the list is palindrome using two pointers tech

--- DSA/task_d/Palindrome.java
+++ DSA/task_d/Palindrome.java
@@ -5,6 +5,8 @@ package task_d;
 
 import java.util.Stack;
 
+import task_b.DoubleLinkedList;
+
 class Node {
     int value;
     Node next;
@@ -62,17 +64,57 @@ public class Palindrome {
         return isPalindromeRecur(head, start);
     }
 
+    static Node reverseList(Node head) {
+        if (head == null || head.next == null) return head;
+        Node curr = head, prev = null, front;
+
+        while (curr != null) {
+            front = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = front;
+        }
+        return prev;
+    }
+
+
+    static boolean isPalindromeUsingTwoPointerTech(Node head) {
+        if (head == null || head.next == null) return true;
+
+        Node fast = head, slow = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next; // reaches up to middle of the list
+            fast = fast.next.next; //reaches up to last of the list
+        }
+
+        Node secondHalf = reverseList((fast == null) ? slow.next : slow);
+        Node firstHalf = head;
+
+        while (secondHalf.next != null) {
+            if (secondHalf.value != firstHalf.value) {
+                secondHalf = reverseList(secondHalf);
+                return false;
+            } else {
+                secondHalf = secondHalf.next;
+                firstHalf = firstHalf.next;
+            }
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
         Node head = new Node(1);
         head.next = new Node(2);
         head.next.next = new Node(3);
-        head.next.next.next = new Node(2);
-        head.next.next.next.next = new Node(1);
+        head.next.next.next = new Node(3);
+        head.next.next.next.next = new Node(2);
+        head.next.next.next.next.next = new Node(1);
 
         boolean result = isPalindrome(head);
 
         System.out.println("is palindrome :" + (result ? "true" : "false"));
-        System.out.println("is palindrome using recursion:" + (result ? "true" : "false"));
+        System.out.println("is palindrome using recursion:" + (isPalindromeList(head) ? "true" : "false"));
+        System.out.println("is palindrome using thw pointer:" + (isPalindromeUsingTwoPointerTech(head) ? "true" : "false"));
     }
 }
