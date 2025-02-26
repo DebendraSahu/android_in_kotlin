773bc9e98de5cbd59af7d096ee21f35f97f998ed
debendra.sahu <debendrasahu150@gmail.com>
2025-02-26 15:59:45 +0530

    Used Optimum Solution.

--- DSA/task_e/MissingNumber.java
+++ DSA/task_e/MissingNumber.java
@@ -2,6 +2,10 @@ package task_e;
 
 
 public class MissingNumber {
+
+
+    // Cons: It can run into integer overflow problems if the numbers or 𝑛 are large,
+    // because the computed sum might exceed the range of the integer type.
     static int missingNumber(int[] numbers) {
         int len = numbers.length + 1;
 //       Sn = (n * (n + 1)) / 2 Gp
@@ -13,11 +17,21 @@ public class MissingNumber {
         return expectedTotal - actualTotal;
     }
 
+    static int missingNumber(int[] a, int n) {
+        int xor1 = 0, xor2 = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            xor2 = xor2 ^ a[i]; // XOR of array elements
+            xor1 = xor1 ^ (i + 1); //XOR up to [1...N-1]
+        }
+        xor1 = xor1 ^ n; //XOR up to [1...N]
+
+        return (xor1 ^ xor2); // the missing number
+    }
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 4, 6, 3, 7, 8, 9, 10, 11};
-        int res = missingNumber(arr);
-
+        int res = missingNumber(arr, 11);
         System.out.print("Missing number is :" + res);
     }
 }
