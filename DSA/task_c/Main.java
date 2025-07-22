ead93611d35e9e85427ca4783b0008be12d7cba2
debendra.sahu <debendrasahu150@gmail.com>
2025-07-22 22:40:57 +0530

    TRIED SOMETHING NEW

--- DSA/task_c/Main.java
+++ DSA/task_c/Main.java
@@ -6,10 +6,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         BinaryTree tree = new BinaryTree();
-        Scanner scanner = new Scanner(System.in);
-        tree.populate(scanner);
-        tree.display();
-        scanner.close();
+        try (Scanner scanner = new Scanner(System.in)) {
+            tree.populate(scanner);
+            tree.display();
+        }
         System.out.println("Max depth of tree: " + tree.getMaxDepth());
         System.out.println("Checking different roots are idetical: " + tree.areIdentical(tree.root, tree.root.left));
     }
--- DSA/task_i/TwoSum.java
+++ DSA/task_i/TwoSum.java
@@ -8,6 +8,16 @@ public class TwoSum {
         int target = -2;
         if (twoSum(arr, target)) System.out.println("true");
         else System.out.println("false");
+        int[] l = {1000000000, 1000000000, 1000000000, 1000000000};
+        int m = 0;
+        for (int i : l) {
+            System.out.println(m);
+            if (Integer.MIN_VALUE - m > i) {       // 2000000000
+                System.out.println("Small value ");// 2147483647
+            }
+            m = m + i;
+        }
+        System.out.println(m);
     }
 
     static boolean twoSum(int[] arr, int target) {
